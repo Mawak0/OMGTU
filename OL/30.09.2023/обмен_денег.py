e2b5f2f506aa12e9a737f8461735d7535f7f44e2
Mawak0 <a32221580@gmail.com>
2023-11-28 18:39:13 +0600

    update обмен_денег

--- OL/30.09.2023/обмен_денег.py
+++ OL/30.09.2023/обмен_денег.py
@@ -1,75 +1,55 @@
-inp = [int(i) for i in input().split()]
-n_i = inp[0] # количество денежных единиц в исходной денежной системе
-c_i = inp[1:n_i] # соотношения между денежными единицами в исходной денежной системе
-
-inp = [int(i) for i in input().split()]
-k_i = inp[0] # количество несчастливых чисел в стране с исходной денежной системой
-A_i = inp[1:k_i+1] # сами несчастливые числа
-
-inp = [int(i) for i in input().split()]
-n_k = inp[0] # количество денежных единиц в конечной денежной системе
-c_k = inp[1:n_k] # соотношения между денежными единицами в конечной денежной системе
-
-inp = [int(i) for i in input().split()]
-k_k = inp[0] # количество несчастливых чисел в стране с конечной денежной системой
-A_k = inp[1:k_k+1] # сами несчастливые числа
-
-cash_i = [int(i) for i in input().split()] # представлние денежной суммы в исходной денежной системе
-
-# print(n_i, c_i)
-# print(k_i, A_i)
-# print(n_k, c_k)
-# print(k_k, A_k)
-# print(cash_i)
-
-def delete_bad_numbers(bad_numbers, money):
-    out = money
-    for n in bad_numbers:
-        if out >= n:
-            out = out-1
-    return out
-
-def add_bad_numbers(bad_numbers, money):
-    out = money
-    for n in sorted(bad_numbers):
-        if out >= n:
-            out = out+1
-    return out
-
-
-def cash_decompiler(multiplers, money, bad_numbers):
-    low_money_out = 0
-    multipler = 1
-    for i in range(len(money)-1, -1, -1):
-        low_money_out += delete_bad_numbers(bad_numbers, money[i])*multipler
-        if i != 0:
-            multipler = multipler * multiplers[i-1]
-    return low_money_out
-
-low_money_i = cash_decompiler(c_i, cash_i, A_i)
-
-def cash_compiler(multiplers, low_money, bad_numbers):
-    def get_current_multipler(multiplers, pos):
-        mult = 1
-        for p in range(pos, len(multiplers)):
-            mult = mult*multiplers[p]
-        return mult
-
-    out_slots = []
-    for i in range(0, len(multiplers)+1):
-        if i != len(multiplers)+1:
-            app = low_money // get_current_multipler(multiplers, i)
-            #print(i, get_current_multipler(multiplers, i), multiplers)
-            low_money = low_money - app * get_current_multipler(multiplers, i)
+def happy(unhappy_nums, num):
+    c = 0
+    for _ in range(len(unhappy_nums)):
+        if num >= unhappy_nums[_]:
+            c += 1
         else:
-            app = low_money
-        out_slots.append(app)
+            break
+    return c
 
-    for i in range(0, len(out_slots)-1):
-        out_slots[i] = add_bad_numbers(bad_numbers, out_slots[i])
-    #out_slots[-1] = delete_bad_numbers(bad_numbers, out_slots[-1])
-    return out_slots
 
-rez = cash_compiler(c_k, low_money_i, A_k)
-print(str(rez)[1:-1].replace(",", ""))
+def to_happy(unhappy_nums, num):
+    c = 0
+    for _ in range(len(unhappy_nums)):
+        if num >= unhappy_nums[_]:
+            num += 1
+            c += 1
+        else:
+            break
+    return c
 
+
+def p(ks):
+    product = 1
+    for k in ks:
+        product *= k
+    return product
+
+
+def func(C_start, start_unhappy, C_end, end_unhappy, start_amount):
+    summ = 0
+    for _ in range(len(start_amount)):
+        num = start_amount[_]
+        summ += (num - happy(start_unhappy, num)) * p(C_start[:len(C_start) - _])
+
+    result = []
+    os = 0
+    for _ in range(len(C_end)):
+        os = summ // p(C_end[:len(C_end) - _])
+        summ -= os * p(C_end[:len(C_end) - _])
+        result.append(os + to_happy(end_unhappy, os))
+    result.append(summ + to_happy(end_unhappy, summ))
+    return " ".join([str(_) for _ in result])
+
+
+
+
+
+C_start = [int(n) for n in input().split(" ")[1:]][::-1]
+start_unhappy = sorted([int(n) for n in input().split(" ")[1:]])
+C_end = [int(n) for n in input().split(" ")[1:]][::-1]
+end_unhappy = sorted([int(n) for n in input().split(" ")[1:]])
+start_amount = [int(n) for n in input().split(" ")]
+
+result = func(C_start, start_unhappy, C_end, end_unhappy, start_amount)
+print(result)
