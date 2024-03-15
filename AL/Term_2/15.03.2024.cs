fb0f3db878cfe9c99e17c90b29cc6cdd2a5fc154
Mawak0 <65796998+Mawak0@users.noreply.github.com>
2024-03-15 23:58:31 +0600

    Update 15.03.2024.cs

--- AL/Term_2/15.03.2024.cs
+++ AL/Term_2/15.03.2024.cs
@@ -14,13 +14,14 @@ class Program
         string[] s;
 
         Dictionary<string, Dictionary<string, Dictionary<string, int>>> first_task_data = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>(); // {date: {number_from: {number_to: count}}}
+        Dictionary<string, Dictionary<string, Dictionary<string, int>>> second_task_data = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>();
         for (int i = 0; i < str_count; i++)
         {
             s = Console.ReadLine().Split(' ');
             string number_from = s[0];
             string number_to = s[1];
             string date = s[2];
-            string talk_time = s[3];
+            int talk_time = int.Parse(s[3]);
 
 
 
@@ -29,18 +30,27 @@ class Program
             if (!first_task_data[date][number_from].ContainsKey(number_to)) { first_task_data[date][number_from].Add(number_to, 0); }
             first_task_data[date][number_from][number_to] = first_task_data[date][number_from][number_to] + 1;
 
+            if (!second_task_data.ContainsKey(date)) { second_task_data.Add(date, new Dictionary<string, Dictionary<string, int>>()); }
+            if (!second_task_data[date].ContainsKey(number_from)) { second_task_data[date].Add(number_from, new Dictionary<string, int>()); }
+            if (!second_task_data[date][number_from].ContainsKey(number_to)) { second_task_data[date][number_from].Add(number_to, talk_time); }
+            if (second_task_data[date][number_from][number_to] < talk_time) { second_task_data[date][number_from][number_to] = talk_time; }
+
         }
 
 
         Console.WriteLine("Введите номер абонента, для получения информации ");
         string need_number = Console.ReadLine();
-        foreach (var e1 in first_task_data) {
+        foreach (var e1 in first_task_data)
+        {
             string data = e1.Key;
-            foreach (var e2 in e1.Value) {
+            foreach (var e2 in e1.Value)
+            {
                 string number_from = e2.Key;
-                if (number_from == need_number) {
+                if (number_from == need_number)
+                {
                     Console.WriteLine($"Статистика за {data}");
-                    foreach (var e3 in e2.Value) {
+                    foreach (var e3 in e2.Value)
+                    {
                         string number_to = e3.Key;
                         int calls_count = e3.Value;
                         Console.WriteLine($"{number_to} - {calls_count} звонков");
@@ -48,6 +58,36 @@ class Program
                 }
             }
         }
-        
+
+        //int m = 0;
+        foreach (var e1 in second_task_data)
+        {
+            string data = e1.Key;
+            foreach (var e2 in e1.Value)
+            {
+                string number_from = e2.Key;
+                int m = 0;
+                string current_num = number_from;
+                string favorite_num_to = "";
+
+                foreach (var e3 in e2.Value)
+                {
+                    string number_to = e3.Key;
+                    int talk_time = e3.Value;
+                    if (m < talk_time)
+                    {
+                        m = talk_time;
+                        favorite_num_to = number_to;
+                    }
+                }
+                Console.WriteLine($"{data} Абонент с номером {current_num} разговаривал c {favorite_num_to} максимум {m} минут");
+
+            }
+
+        }
+
+
+
+        Console.ReadKey();
     }
 }
