0e89467c5c0f28373dfe2a5af59d52554d8703c5
Mawak0 <a32221580@gmail.com>
2024-04-24 10:19:18 +0600

    update шестерни

--- OL/Term_2/шестеренки(NR).py
+++ OL/Term_2/шестеренки(NR).py
@@ -2,32 +2,67 @@
 
 
 def Prog(mode, input_path=None, output_path=None):
+    gears = []
+    connections = []
     if mode == 'file':
         f = open(input_path, "r")
         N, M = [int(q) for q in f.readline().strip().split(" ")]
-        gears = []
         for i in range(0, N):
             gears.append([int(q) for q in f.readline().strip().split(" ")])
-        connections = []
         for i in range(0, M):
-            connections.append([int(q) for q in f.readline().strip().split(" ")])
-        Z1, Z2 = [int(q) for q in f.readline().strip().split(" ")]
+            connections.append([int(q)-1 for q in f.readline().strip().split(" ")])
+        Z1, Z2 = [int(q)-1 for q in f.readline().strip().split(" ")]
         V = int(f.readline().strip())
         f.close()
-    if mode == "manual":
+    else:
         N, M = [int(q) for q in input().split(" ")]
-        gears = []
         for i in range(0, N):
             gears.append([int(q) for q in input().split(" ")])
-        connections = []
         for i in range(0, M):
-            connections.append([int(q) for q in input().split(" ")])
-        Z1, Z2 = [int(q) for q in input().split(" ")]
+            connections.append([int(q)-1 for q in input().split(" ")])
+        Z1, Z2 = [int(q)-1 for q in input().split(" ")]
         V = int(input())
+    gears.sort()
+    connections.sort()
+    gears[Z1].append(V)
+    for i in range(0, len(gears)):
+        if i != Z1:
+            gears[i].append(0)
+    def sig(a):
+        if a > 0:
+            return 1
+        if a < 0:
+            return -1
+        else:
+            return 0
+    print(Z1, Z2)
 
+    for i in range(0, len(connections)):
+        if (gears[connections[i][0]][2] != 0) and (gears[connections[i][1]][2] == 0):
+            diff = gears[connections[i][0]][1] / gears[connections[i][1]][1]
+            gears[connections[i][1]][2] = - gears[connections[i][0]][2]*diff
 
+        elif (gears[connections[i][0]][2] == 0) and (gears[connections[i][1]][2] != 0):
+            diff = gears[connections[i][1]][1] / gears[connections[i][0]][1]
+            gears[connections[i][0]][2] = - gears[connections[i][1]][2] * diff
 
-    out_str = ""
+        elif (gears[connections[i][0]][2] != 0) and (gears[connections[i][1]][2] != 0):
+            if sig(gears[connections[i][0]][2]) == sig(gears[connections[i][1]][2]):
+                gears[Z2][2] = 0
+                break
+
+    print(gears)
+    print(connections)
+
+    if gears[Z2][2] != 0:
+        q = str(abs(round(gears[Z2][2], 3)))
+
+        while len(q.split(".")[1]) < 3:
+            q = q + "0"
+
+        out_str = "1\n"+str(sig(gears[Z2][2]))+"\n"+q
+    else:
+        out_str = "-1"
 
 
     if mode == 'auto':
@@ -48,8 +83,8 @@ if autocheck:
         if len(q) == 1:
             q = "0" +q
         print(q)
-        out = Prog(mode="file", input_path="C:/Users/User/Downloads/Игра ~Города~/input_s1_"+q+".txt", output_path="output.txt")
-        f = open("C:/Users/User/Downloads/Игра ~Города~/output_s1_" + q +".txt")
+        out = Prog(mode="file", input_path="C:/Users/user/Downloads/Шестренки/input_s1_"+q+".txt", output_path="output.txt")
+        f = open("C:/Users/user/Downloads/Шестренки/output_s1_" + q +".txt")
         u = f.read()
         if out != u:
             print("ERROR")
@@ -58,5 +93,5 @@ if autocheck:
             print(out)
 
 else:
-    print(Prog(mode="file", input_path="C:/Users/user/Downloads/Игра ~Города~/input_s1_04.txt", output_path="output.txt"))
+    print(Prog(mode="file", input_path="C:/Users/user/Downloads/Шестренки/input_s1_08.txt", output_path="output.txt"))
     # print(Goroda(mode='manual'))
