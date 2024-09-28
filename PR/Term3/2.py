576b495f5d5d0f445b1e7002bbf626e9d61655b7
Mawak0 <a32221580@gmail.com>
2024-09-28 16:35:37 +0600

    update

--- PR/Term3/2.py
+++ PR/Term3/2.py
@@ -1,24 +1,60 @@
 from scipy.misc import derivative
 import scipy
 import sympy
-def f(x):
-    return (2*x**3)**(1/2)
+import math
 
-p1 = derivative(f, 4.0, dx=1e-6)
+def f(x):
+    return 2/(math.sin(x)+4)
+
+p1 = derivative(f, 2.0, dx=1e-6)
 
 print("Первая производная функции в точке: "+str(p1))
 
-p2= derivative(f, 4.0,n=2, dx=1e-6)
+p2= derivative(f, 2.0,n=2, dx=1e-6)
 
 print("Вторая производная функции в точке: "+str(p2))
 x = sympy.Symbol('x')
-ps = sympy.diff((2*x**3)**(1/2), x)
+ps = sympy.diff(2/sympy.sin(x)+4, x)
+print("Символьное представление производной:")
 print(ps)
 
 
-iq = scipy.integrate.quad(f, a = 2, b= 4)
+iq = scipy.integrate.quad(f, a = 3, b= 6)
 
 print("определенный интеграл от a до b = "+str(iq[0]))
 
-it = sympy.integrate((2*x**3)**(1/2), x)
+it = sympy.integrate(2/sympy.sin(x)+4, x)
 print("Неопределенный интеграл "+str(it))
+
+def L(X):
+    x1, x2 = X
+    return (x1-4)**2 + (x2-2)**2
+
+print("Минимизация функции:")
+
+def b1(X):
+    x1, x2 = X
+    return 4*x1 + 2*x2 - 11
+
+def b2(X):
+    x1, x2 = X
+    return -2*x1 - 7
+
+con1 = {'type': 'ineq', 'fun': b1}
+con2 = {'type': 'ineq', 'fun': b2}
+cons = [con1, con2]
+
+bounds = [(0, None), (0, None)]
+
+
+x0 = [0.5, 0.5]
+
+solution = scipy.optimize.minimize(L, x0, method='SLSQP', bounds=bounds, constraints=cons)
+
+print('Решение:', solution.x)
+print('Значение целевой функции:', solution.fun)
+
+
+
+
+
