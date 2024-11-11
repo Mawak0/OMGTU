92211e161acc564ff1181c21b3b163415033c746
Mawak0 <65796998+Mawak0@users.noreply.github.com>
2024-11-11 16:54:14 +0600

    Update linux_1.c

--- OP_S/Term3/Lab2/linux_1.c
+++ OP_S/Term3/Lab2/linux_1.c
@@ -8,11 +8,11 @@ int main()
 {
     int standart_input_d = 0;
     int standart_output_d = 1;
-    char b1[] = "Enter any text (15 bytes): ";
+    char b1[] = "Enter any text: ";
     write(standart_output_d, b1, strlen((const char*)b1));
-    char b2[16];
-    read(standart_input_d, b2, 15);
-    b2[15] = '\0';
+    char b2[30];
+    read(standart_input_d, b2, 29);
+    b2[29] = '\0';
     int file = open("test_file", O_CREAT | O_WRONLY | O_TRUNC);
     write(file, b2, strlen((const char*)b2));
     close(file);
